60dbb252000d90b2e060823db5a0354d9bb791a5
Linact2 <linact2.hotmail.co.uk>
2019-10-14 19:26:12 +0100

    ghappy.test file

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,19 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "{0}: ({1} = {2}")
+    @CsvSource({
+            // one works one doesn't
+            "'Works true', 'xxggxx', true", "'not work', xgxgxg, false", "'operator', x, false"
+    })
+
+    public void testAlgorithm(String para, String happy, Boolean bool){
+        Boolean result = new GHappy().gHappy(happy);
+        Assertions.assertEquals(result, bool);
+    }
 }
